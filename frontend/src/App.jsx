a2a10f0293429dbb5d04405cde9d8a3e0b733d75
Bharath s <devbharu04@gmail.com>
2025-08-23 00:17:46 +0530

    chatbot commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,21 +8,27 @@ import ThreatsGuide from './pages/ThreatsGuide.jsx';
 import Course from './pages/home/Course.jsx';
 import Products from './pages/home/Products.jsx';
 import Profile from './pages/home/Profile.jsx';
-
+import Chatbot from './components/Chatbot.jsx'
 
 function App() {
   return (
-    <div className="text-white min-h-screen" style={{ background: 'transparent' }}>
-      <Routes>
+    <div>
+       <Chatbot/>
+        <div className="text-white min-h-screen" style={{ background: 'transparent' }}>
+     
+     <Routes>
          <Route path="/" element={<CyberSecurityNewsPage />} />
-        <Route path="/news" element={<CyberSecurityNewsPage />} />
-  <Route path="/home" element={<Home />} />
-  <Route path="/course" element={<Course />} />
-  <Route path="/products" element={<Products />} />
-  <Route path="/profile" element={<Profile />} />
-        <Route path="/guide" element={<ThreatsGuide />} />
-      </Routes>
+         <Route path="/news" element={<CyberSecurityNewsPage />} />
+         <Route path="/home" element={<Home />} />
+         <Route path="/course" element={<Course />} />
+         <Route path="/products" element={<Products />} />
+         <Route path="/profile" element={<Profile />} />
+         <Route path="/guide" element={<ThreatsGuide />} />
+     </Routes>
+   </div>
     </div>
+   
+    
   );
 }
 
