1af653509ea0097e788739fef8fe9a68a25e5f7d
deekshithgowda85 <152056807+deekshithgowda85@users.noreply.github.com>
2025-08-22 20:48:20 +0530

    First commit clerk

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import CyberSecurityNewsPage from './pages/CyberSecurityNewsPage.jsx';
+import Home from './pages/Home.jsx';
+
+
+function App() {
+  return (
+    <div className="text-white min-h-screen" style={{ background: 'transparent' }}>
+      <Routes>
+        <Route path="/" element={<CyberSecurityNewsPage />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App
