716138845b6288071592be342e8d6ef1735de932
Bharath s <devbharu04@gmail.com>
2025-08-23 17:14:34 +0530

    hi

--- backend/server.js
+++ backend/server.js
@@ -9,23 +9,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const allowedOrigins = [
-  "https://securex-innovatrix.vercel.app", // frontend origin
-  "http://localhost:3000" // optional, for local dev
-];
+app.use(cors());
 
-app.use(cors({
-  origin: function(origin, callback) {
-    // allow requests with no origin (like Postman)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true
-}));
 
 const NEWS_API_KEY = process.env.NEWS_API_KEY  ;
  
